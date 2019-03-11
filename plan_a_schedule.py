75282ddf475301cfde9cf6bd674bbb9632e40364
jacob975 <z123a123s123@gmail.com>
2019-03-11 16:01:02 +0000

    Minor fix

--- plan_a_schedule.py
+++ plan_a_schedule.py
@@ -14,6 +14,7 @@ update log
 import time
 from astropy.time import Time
 from sys import argv
+from datetime import datetime
 
 #--------------------------------------------
 # Main code
@@ -24,27 +25,38 @@ if __name__ == "__main__":
     # Load argv
     if len(argv) != 4:
         print "Wrong number of arguments."
-        print "Usage: plan_a_schedule.py [midpointjd] [period] [transitduration]"
+        print "Usage: plan_a_schedule.py [midpointjd] [period in days] [transitduration in hours]"
         exit()
     midpointjd = float(argv[1])
     period = float(argv[2])
     transitduration = float(argv[3])
-    count = 0
+    #----------------------------------------
+    # Get the JD of today
+    Today = Time(datetime.utcnow(), scale='utc', location=('120d', '40d'))
+    print ("The timing of now in iso format:")
+    print (Today.iso)
+    Today_in_jd = Time(Today.jd, format = 'jd', scale = 'utc')
+    print ("In Julian date:")
+    print (Today_in_jd)
+    print ("In UTC:")
+    print (Today_in_jd.iso)
+    num_period = int((float(Today.jd) - midpointjd)/period)
+    midpointjd = midpointjd + float(num_period) * period
     # print the plan
-    print 'start\t\t\tend'
+    print 'start\t\t\t\t\tend'
+    count = 0
     while True:
         # escape condition
-        if period * count > 31:
+        if count > 11:
             break
         # find the middle point
         curr_midpoint = midpointjd + count * period
         curr_startpoint = curr_midpoint - transitduration/48
         curr_stoppoint = curr_midpoint + transitduration/48
-        curr_startpoint = Time(curr_startpoint, format = 'jd', scale = 'ut1')
-        curr_stoppoint = Time(curr_stoppoint, format = 'jd', scale = 'ut1')
-        curr_startpoint.format = 'iso'
-        curr_stoppoint.format = 'iso'
-        print '{0}, {1}'.format(curr_startpoint.ut1, curr_stoppoint.ut1)
+        curr_startpoint = Time(curr_startpoint, format = 'jd')
+        curr_stoppoint = Time(curr_stoppoint, format = 'jd')
+        print '{0}, {1}'.format(curr_startpoint.iso, curr_stoppoint.iso)
+        #print '{0}, {1}'.format(curr_startpoint.jd, curr_stoppoint.jd)
         count += 1
     #---------------------------------------
     # Measure time
